580df68781c441ea488c95a19b087153ea888bc5
sachinjain-hub <sj314195@gmail.com>
2025-07-13 19:01:41 +0530

    app.py

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-from flask import Flask, render_template, request,jsonify
-import pickle
+from flask import Flask, request, jsonify
 import os
+import pickle
 import numpy as np
-os.environ.get("PORT", 5000)
+
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
 @app.route('/predict', methods=['POST'])
 def predict():
-    features = [float(x) for x in request.form.values()]
+    data = request.get_json(force=True)
+    features = [data['area'], data['bedrooms'], data['bathrooms'], data['stories'],
+                data['mainroad'], data['guestroom'], data['basement'],
+                data['hotwaterheating'], data['airconditioning'],
+                data['parking'], data['prefarea'], data['furnishingstatus']]
+    
     prediction = model.predict([np.array(features)])
     return jsonify({'predicted_price': int(prediction[0])})
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # This is required for Render
+    app.run(host='0.0.0.0', port=port)
